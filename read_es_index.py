cf0d447702aec8bce1a0557c5bd4466e3e65c149
Alexander Marquardt <alexander.marquardt@elastic.co>
2019-06-03 00:05:51 +0100

    Removed unused code

--- read_es_index.py
+++ read_es_index.py
@@ -32,22 +32,6 @@ def scroll_over_all_docs(index_name):
         logging.exception(e)
 
 
-def loop_over_hashes_and_remove_duplicates():
-    # Search through the hash of doc values to see if any
-    # duplicate hashes have been found
-    for hashval, array_of_ids in dict_of_duplicate_docs.items():
-        if len(array_of_ids) > 1:
-            print("********** Duplicate docs hash=%s **********" % hashval)
-            # Get the documents that have mapped to the current hasval
-            matching_docs = es.mget(index="stocks", doc_type="doc", body={"ids": array_of_ids})
-            for doc in matching_docs['docs']:
-                # In order to remove the possibility of hash collisions,
-                # write code here to check all fields in the docs to
-                # see if they are truly identical - if so, then execute a
-                # DELETE operation on all except one.
-                # In this example, we just print the docs.
-                print("doc=%s\n" % doc)
-
 
 def read_all_docs_from_es_index_and_persist_to_disk(index_name):
     scroll_over_all_docs(index_name)
